77d27e65430a128fb523ac230b3eddb1b09a1be2
somayehhosseinbeig <somayeh.hosseinbeig@gmail.com>
2026-05-29 16:46:38 +0330

    live server

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -54,10 +55,12 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'mywebsite.urls'
 
+# import os
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
+        #[os.path.join(BASE_DIR,'templates')], must be import os first
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
